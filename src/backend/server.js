47a4f70bcb684a20147fecb0e51c104d62f099a0
fuselierr <mikegao000@gmail.com>
2025-01-25 14:38:40 -0500

    setup basic server

--- src/backend/server.js
+++ src/backend/server.js
@@ -4,4 +4,10 @@ const PORT = 3001;
 const app = express();
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Server is up and running!');
+})
 
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
