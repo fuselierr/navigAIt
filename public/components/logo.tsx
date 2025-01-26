61bd3a15771416d29fea3d7e00e2cfebf4fdb590
Daniel <97817895+dnl-wu@users.noreply.github.com>
2025-01-25 20:00:24 -0500

    Create logo.tsx

---
+++ public/components/logo.tsx
@@ -0,0 +1,9 @@
+export default function Logo() {
+  return (
+    <h1 className="text-4xl mb-8 text-[#87CEEB] text-center font-bold leading-tight">
+      Elevate Your
+      <br />
+      Onboarding Process
+    </h1>
+  )
+}
